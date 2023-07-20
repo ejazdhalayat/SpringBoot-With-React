3b72f88075cb5defa130a21bab0960a9a016c981
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2023-07-20 17:50:04 +0530

    created employeeservice

--- src/components/ListEmployee.jsx
+++ src/components/ListEmployee.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 
 
 function ListEmployee() {
@@ -7,6 +8,30 @@ function ListEmployee() {
   return (
     <div className='container'>
         <h2 className='text-center'>ListEmployee</h2>
+
+        {/* Employee Table */}
+        <table className='table table-bordered table-striped'>
+            <thead>
+                <th>Employee Id</th>
+                <th>Employee First Name</th>
+                <th>Employee Last Name</th>
+                <th>Employee Email ID</th>
+            </thead>
+            <tbody>
+                {
+                    employees.map(
+                        employee =>
+                        <tr key = {employee.id}>
+                            <td> {employee.id} </td>
+                            <td> {employee.firstName} </td>
+                            <td> {employee.lastName} </td>
+                            <td> {employee.emailId} </td>
+
+                        </tr>
+                    )
+                }
+            </tbody>
+        </table>
 
     </div>
   )
---
+++ src/services/EmployeeService.jsx
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const REST_API = 'http://localhost:8080/api/v1/employees';
+
+class EmployeeService{
+
+    getAllEmployees(){
+        return axios.get(REST_API)
+    }
+}
