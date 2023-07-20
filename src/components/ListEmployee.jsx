5a3e35fc8c7d507b71c6a56ba68b30db555e0bff
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2023-07-20 14:40:38 +0530

    ListEmployee Component

---
+++ src/components/ListEmployee.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function ListEmployee() {
+    const [employees, setEmployees] = useState([])
+
+  return (
+    <div className='container'>
+        <h2 className='text-center'>ListEmployee</h2>
+
+    </div>
+  )
+}
+
+export default ListEmployee
