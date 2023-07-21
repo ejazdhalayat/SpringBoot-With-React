0378aa50e85ea1d62a8070c053fb64ecfe80a0e4
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2023-07-21 09:37:06 +0530

    created HeaderComponent

---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function HeaderComponent() {
+  return (
+    <div>
+        <heade>
+            <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
+                <div>
+                    <a href='' className='navbar-brand'>
+                        Employee Management
+                    </a>
+                </div>
+
+            </nav>
+        </heade>
+    </div>
+  )
+}
+
+export default HeaderComponent
