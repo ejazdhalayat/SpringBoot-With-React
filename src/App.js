73dde82928963d92625604fdcf0df686482db88a
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2023-07-21 16:01:06 +0530

    footercomponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import HeaderComponent from './components/HeaderComponent';
 import ListEmployee from './components/ListEmployee';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
     <div>
+      <HeaderComponent />
       <ListEmployee />
+      <FooterComponent />
      
     </div>
   );
--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function FooterComponent() {
   return (
     <div>
-        <footer className='footer'>
+        <footer className="footer">
             <span className='text-muted'>
                 All Rights Reserved 2023
             </span>
