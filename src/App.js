cc83e5c71678b6f60c05fb7a8a131145aeb4f597
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2023-07-21 18:20:16 +0530

    Added reacr router dom in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 import HeaderComponent from './components/HeaderComponent';
 import ListEmployee from './components/ListEmployee';
 import FooterComponent from './components/FooterComponent';
@@ -7,9 +9,18 @@ import FooterComponent from './components/FooterComponent';
 function App() {
   return (
     <div>
+      <Router>
       <HeaderComponent />
-      <ListEmployee />
+      <div className='container'>
+          <Routes>
+            <Route exact path='/' Component={ListEmployee}></Route>
+            <Route path='/employees' Component={ListEmployee}></Route>
+          </Routes>
+           
+            
+      </div>
       <FooterComponent />
+      </Router>
      
     </div>
   );
